02da78277cb10c9ff6e99e2e3d65568ef6cc3eaf
Ashu2612 <versecesanu2202@gmail.com>
2023-10-06 11:01:57 +0530

    Icons added.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Storage.Pickers;
 
 namespace TaskTrackPro
 {
@@ -14,9 +18,47 @@ namespace TaskTrackPro
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated(window =>
+                    {
+                        IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                        AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
 
+                        //hide the minimize and maximize button in title bar
+                        window.ExtendsContentIntoTitleBar = true;
+                        if (winuiAppWindow.Presenter is OverlappedPresenter p)
+                        {
+                            p.IsResizable = true;
+                            p.IsMaximizable = false;
+                            p.SetBorderAndTitleBar(false, false);
+                        }
+                        winuiAppWindow.Closing += async (s, e) =>
+                        {
+                            e.Cancel = true;
+
+                            bool result = await App.Current.MainPage.DisplayAlert(
+                        "Close the application ?",
+                        "Are sure you want to quite ?",
+                        "Yes",
+                        "No");
+
+                            if (result)
+                            {
+                                App.Current.Quit();
+                            }
+
+                        };
+                    });
+                });
+            });
+#endif
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
